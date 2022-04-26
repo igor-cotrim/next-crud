454edf123187c2b9031ee8959afbfe6ff5a2ed06
Igor Cotrim <igorxuxicotrim@gmail.com>
2022-04-25 23:19:52 -0300

    config prettier and eslint

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,18 @@
-import Title from "../Title"
+import Title from '../Title'
 
 type LayoutProps = {
   title: string
   children: React.ReactNode
 }
 
-const Layout = ({children,title}:LayoutProps) => {
+const Layout = ({ children, title }: LayoutProps) => {
   return (
-    <div className={`
+    <div
+      className={`
       flex flex-col w-2/3
       bg-white text-gray-800 rounded-md
-    `}>
+    `}
+    >
       <Title>{title}</Title>
       <div className={`p-6`}>{children}</div>
     </div>
